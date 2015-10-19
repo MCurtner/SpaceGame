b97c77b60b8c831a47236cae1e7442de53095fd5
Matthew <Kinematic12@gmail.com>
2015-10-19 18:32:02 +0200

    Refactoring to remove code duplication

--- SpaceGame/GameScene.swift
+++ SpaceGame/GameScene.swift
@@ -13,20 +13,24 @@ class GameScene: SKScene {
     let stars = SKSpriteNode(imageNamed: "stars")
     let stars2 = SKSpriteNode(imageNamed: "stars")
     let planet = SKSpriteNode(imageNamed: "planetBig")
+    let farPlanets = SKSpriteNode(imageNamed: "farPlanets")
+    let farPlanets2 = SKSpriteNode(imageNamed: "farPlanets")
     let planet2 = SKSpriteNode(imageNamed: "planetRing")
     let starShip = SKSpriteNode(imageNamed: "Spaceship")
+    
     
     override func didMoveToView(view: SKView) {
 
         // Add Background
         setupBackground()
        
-        // Add Stars
-        setupStars()
+        // Add Stars and Far Planets
+        setupOverlays(stars, sprite2: stars2, zPos: 0)
+        setupOverlays(farPlanets, sprite2: farPlanets2, zPos: 1)
         
         // Add Planets
         setupPlanets(planet)
-        setupPlanets(planet2)
+        
         
         // Add Ship
         starShip.size = CGSize(width: 100, height: 100)
@@ -41,43 +45,18 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
 
-        moveStarDown()
+        moveDown(stars, sprite2: stars2, speed: 0.5)
+        moveDown(farPlanets, sprite2: farPlanets2, speed: 0.7)
         movePlanetsDown()
-        
-        
-        /*
-            Place the stars overlay at the top of stars2 image
-            once it's position is less than the -height of iteself
-        */
-        if stars.position.y < -stars.size.height {
-            stars.position.y = stars2.size.height + 10
-        }
-        if stars2.position.y < -stars2.size.height {
-            stars2.position.y = stars.size.height + 10
-        }
-        
-        
-        /*
-            If the planet image position.y is less than
-            stars overlay height, remove it from the parent
-            and create a new planet at the top of the screen
-        */
-        if planet.position.y < -stars.size.height {
-            planet.removeFromParent()
-            
-            setupPlanets(planet)
-            
-            planet.position.y = stars.size.height + 10
-        }
-        
     }
+    
+    
     
     // MARK: Setup Methods
     
     /*
         Setup and add the background to the scene
     */
-    
     func setupBackground() {
         let background = SKSpriteNode(imageNamed: "Background")
         background.position = CGPointMake(self.size.width/2, self.size.height/2)
@@ -90,27 +69,39 @@ class GameScene: SKScene {
         Setup and add the two star overlays the scene. The second 
         ontop of the other in order to repeat.
     */
-    func setupStars() {
-        // Add Stars
-        stars.position = CGPointMake(self.size.width/2, self.size.height/2)
-        stars.zPosition = 0
-        addChild(stars)
+    func setupOverlays(sprite1: SKSpriteNode, sprite2: SKSpriteNode, zPos: CGFloat) {
+        // Add overlay1
+        sprite1.position = CGPointMake(self.size.width/2, self.size.height/2)
+        sprite1.zPosition = zPos
+        addChild(sprite1)
         
-        stars2.position = CGPointMake(self.size.width/2, stars.size.height + 10)
-        stars2.zPosition = 0
-        addChild(stars2)
+        // Add overlay2
+        sprite2.position = CGPointMake(self.size.width/2, sprite1.size.height + 10)
+        sprite2.zPosition = zPos
+        addChild(sprite2)
     }
     
     
     /*
-        Move the stars overlay down at a rate of 1 downward for ever 1/60
-        of a second
+        Move the sprite images down at a rate of set speed downward for every
+        1/60 of a second
     */
-    func moveStarDown() {
-        stars.position = CGPoint(x: stars.position.x, y: stars.position.y - 1)
-        stars2.position = CGPoint(x: stars2.position.x, y: stars2.position.y - 1)
+    func moveDown(sprite1: SKSpriteNode, sprite2: SKSpriteNode, speed: CGFloat) {
+        sprite1.position = CGPoint(x: sprite1.position.x, y: sprite1.position.y - speed)
+        sprite2.position = CGPoint(x: sprite2.position.x, y: sprite2.position.y - speed)
+        
+        /*
+        Place the sprite1 overlay at the top of sprite2 image
+        once it's position is less than the -height of iteself
+        */
+        if sprite1.position.y < -sprite1.size.height {
+            sprite1.position.y = sprite2.size.height + 10
+        }
+        if sprite2.position.y < -sprite2.size.height {
+            sprite2.position.y = sprite1.size.height + 10
+        }
+
     }
-    
     
     /*
         Setup the planet CGSize to be a radom number between
@@ -136,7 +127,7 @@ class GameScene: SKScene {
         Setup and add the planet to the scene
     */
     func setupPlanets(planet: SKSpriteNode) {
-        planet.zPosition = 1
+        planet.zPosition = 2
         planet.size = radomizePlanetSize()
         planet.position = randomPlanetPosition(UInt32(self.frame.size.width), screenHeight: UInt32(self.frame.size.height) * 2)
         addChild(planet)
@@ -147,8 +138,22 @@ class GameScene: SKScene {
         1/60 of a second
     */
     func movePlanetsDown() {
-        planet.position = CGPoint(x: planet.position.x, y: planet.position.y - 5)
-        planet2.position = CGPoint(x: planet2.position.x, y: planet2.position.y - 5)
+        planet.position = CGPoint(x: planet.position.x, y: planet.position.y - 3)
+        planet2.position = CGPoint(x: planet2.position.x, y: planet2.position.y - 3)
+        
+        /*
+        If the planet image position.y is less than
+        stars overlay height, remove it from the parent
+        and create a new planet at the top of the screen
+        */
+        if planet.position.y < -stars.size.height {
+            planet.removeFromParent()
+            
+            setupPlanets(planet)
+            
+            planet.position.y = stars.size.height + 10
+        }
     }
+
     
 }
